746a754f369dfb56d0f68b1934dbb58e32622aaa
Salikhova Nazgul <nazgulka2005@gmail.com>
2023-10-30 14:19:05 +0300

    Update InteriorPoint.py

--- Interior_Point_Algo/InteriorPoint.py
+++ Interior_Point_Algo/InteriorPoint.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-
-def interior_point_algorithm(main_function: np.array, constraints_matrix: np.array, constraints_vector: np.array,
-                             init_point: np.array, alpha) -> None:
+def interior_point_algorithm(main_function: np.array, constraints_matrix: np.array, init_point: np.array, a) -> None:
     c = main_function
     A = constraints_matrix
     D = np.diag(init_point)
@@ -24,13 +22,13 @@ def interior_point_algorithm(main_function: np.array, constraints_matrix: np.arr
         if min_neg >= 0:
             break
         v = abs(np.min(c_proj))
-        y = np.ones(A_prime_def_inverse.shape[0]) + alpha/v * c_proj
+        y = np.ones(A_prime_def_inverse.shape[0]) + a / v * c_proj
 
         x = D @ y
-        D_prev = D
-        answ = x
+        # D_prev = D
+        answer = x
         print("result:")
-        print(answ)
+        print(answer)
         D = np.diag(x)
         # diff = D-D_prev
         # r = max(np.amax(diff), np.amin(diff), key=abs)
@@ -38,19 +36,45 @@ def interior_point_algorithm(main_function: np.array, constraints_matrix: np.arr
         r = np.linalg.norm(diff, ord=2)
         n += 1
 
+def prepare_for_algorithm(c_array, A_array, b_vector, x):
+    n = len(b_vector)
+    c_array = np.append(c_array, [0] * n)
+    # дописать как найти инишл
+    x = 
+    print(f"Feasible initial solution: {x}")
+    return c_array, x
+
 
 if __name__ == "__main__":
-    mf = np.array([1, 2, 0])
-    constr = np.array([1, 1, 1])
-    in_p = np.array([2, 2, 4])
-    free_v = np.array([8])
+    # ПРИМЕРЫ
+    # x =
+    # A = np.array([[6, 15, 6, 1], [14, 42, 16, 1], [2, 8, 2, 1]], float)
+    # c = np.array([10, -7, -5], float)
 
-    # x = np.array([1/2, 7/2, 1, 2])
-    # A = np.array([[2, 4, 1, 0], [1, 3, 0, -1]])
-    # c = np.array([1, 1, 0, 0])
-    x = np.array( [ 1 , 1 , 1 , 315 , 174 , 169])
-    A = np.array([[18, 15, 12, 1, 0, 0], [6, 4, 8, 0, 1, 0], [5, 3, 3, 0, 0, 1]], float)
-    c = np.array([9, 10, 16, 0, 0, 0], float)
+    # x = np.array( [ 1 , 1 , 1 , 315 , 174 , 169])
+    # A = np.array([[18, 15, 12, 1, 0, 0], [6, 4, 8, 0, 1, 0], [5, 3, 3, 0, 0, 1]], float)
+    # c = np.array([9, 10, 16, 0, 0, 0], float)
 
-    #interior_point_algorithm(mf, constr, free_v, in_p, 0.5)
-    interior_point_algorithm(c, A, [], x, 0.5)
+    # НАЧАЛО ИНПУТА
+    # c = np.array(list(map(float, input("Enter the coefficients of the function:\n").split())))
+    # A = []
+    # num_of_constraints = int(input("How many constraints?\n"))
+    # print("Enter the coefficients of variables in constraints:")
+    # for i in range(num_of_constraints):
+    #     A.append(list(map(float, input().split())))
+    # A = np.array(A)
+    # print("Enter the variables of right-hand side numbers in constraints:")
+    # b = np.array(list(map(float, input().split())))
+    #
+    # approximation_accuracy = int(input("Enter the approximation accuracy:\n"))
+    
+    # дописать как к А добавить коэффициенты free variables 
+    A = np.array([[2, 4, 1, 0], [1, 3, 0, -1]], float)
+    c = np.array([1, 1], float)
+    b = np.array([16, 9], float)
+    alpha = 0.5
+    x = []
+    A_copy = np.copy(A)
+    c, x = prepare_for_algorithm(c, A_copy, b, x)
+    # запуск алгоритма
+    # interior_point_algorithm(c, A, x, alpha)
